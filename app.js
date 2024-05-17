faf991c5c5fb59dd54bbf935a7033c4eaae5a5ec
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-05-17 23:24:58 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let apiKey = '7c21d020c7be4608b7816af5393971dd';
+let url = 'https://newsapi.org/v2/everything?q=tesla';
+
+
+async function news(){
+    let response = await fetch(url + `&apiKey=${apiKey}`)
+    let data = await response.json();
+    console.log(data);
+}
+news()
