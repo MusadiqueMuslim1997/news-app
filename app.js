7f0645f10f1aab0cda95a65fadba4b7648ee8153
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-05-18 22:28:26 +0500

    add

--- app.js
+++ app.js
@@ -4,10 +4,12 @@
 let apiKey = 'pub_4443486bab1cb3f1d1124664c510cf1cda6f7';
 let url = 'https://newsapi.org/v2/everything?q=pakistan';
 
-
-async function news(){
-    let response = await fetch(`https://newsdata.io/api/1/latest?apikey=pub_4443486bab1cb3f1d1124664c510cf1cda6f7&q=pakistan&language=en`)
+let vid = document.getElementById("vid")
+async function news() {
+    let response = await fetch(`https://newsdata.io/api/1/news?apikey=pub_4443486bab1cb3f1d1124664c510cf1cda6f7&q=cricket `)
     let data = await response.json();
     console.log(data);
+    vid.innerHTML = data.results[0].source_url;
+
 }
 news()
