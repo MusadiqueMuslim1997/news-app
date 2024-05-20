2af0e1b9635c98b3b4b48f81d3a577f7753f7d8e
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-05-20 23:08:19 +0500

    add

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let card = document.getElementById('card');
         let searchInput = document.getElementById('search');
 
         async function news(title) {
-            let response = await fetch(`https://newsdata.io/api/1/news?apikey=pub_4443486bab1cb3f1d1124664c510cf1cda6f7&q=${title}`);
+            let response = await fetch(`https://newsdata.io/api/1/latest?apikey=pub_4443486bab1cb3f1d1124664c510cf1cda6f7&q=` + title);
             let data = await response.json();
             console.log(data);
 
@@ -18,7 +18,8 @@ let card = document.getElementById('card');
                 <div class="card">
                     <div class="iframe"><img src="${element.image_url}" alt="News Image"></div>
                     <div class="channel">${element.country}</div>
-                    <div class="content">${element.title}</div>
+                    <div class="content">${element.creator}</div>
+                    <button class = "btn1"><a href="${element.link}" target="_blank">View Site</a></button>
                 </div>`;
             });
         }
@@ -28,4 +29,4 @@ let card = document.getElementById('card');
         }
 
         // Call news with a default value on page load
-        news('news');
+        news('sports');
